d4a1b691e0b378c536c3a3c627cc53788817349a
Bhrugu14 <knowbhrugu14@gmail.com>
2023-03-15 03:25:09 +0530

    clear all data

--- src/screen/editor/editorCanvas.tsx
+++ src/screen/editor/editorCanvas.tsx
@@ -3,16 +3,15 @@ import { DragBox } from "../../component";
 import { useBoxContext } from "../../context";
 import { getWithExpiry, setWithExpiry } from "../../utils/storeData";
 
+interface BoxesProps {
+  x?: number;
+  y?: number;
+  data?: any;
+}
+
 export const EditorCanvas = (props) => {
   const boxCtx = useBoxContext();
-  const [boxes, setBoxes] = useState([
-    {
-      id: new Date().toString(),
-      x: 50,
-      y: 50,
-      data: "button",
-    },
-  ]);
+  const [boxes, setBoxes] = useState<BoxesProps[] | any>([]);
   const [extra, setExtra] = useState(0);
 
   useEffect(() => {
@@ -22,38 +21,34 @@ export const EditorCanvas = (props) => {
       console.log("savedPosition", savedPosition);
       if (savedPosition) {
         setBoxes(savedPosition);
-      } else {
-        setBoxes([
-          {
-            id: new Date().toDateString(),
-            x: 50,
-            y: 50,
-            data: "button",
-          },
-        ]);
+        setExtra(extra + 1);
       }
+    } else {
+      setBoxes([]);
+      setExtra(extra + 1);
     }
   }, [boxCtx?.extra]);
 
   return (
     <div className="relative h-full w-full float-left">
       <div className="h-full w-full bg-background">
-        {boxes.map((box, k) => (
-          <DragBox
-            key={"preview" + box.id + k}
-            x={box.x}
-            y={box.y}
-            onStop={(e, { x, y }) => {
-              boxes[k].x = x;
-              boxes[k].y = y;
-              setBoxes(boxes);
-              setWithExpiry("position", boxes);
-              // dispatch(setBoxComponents(boxes));
-              setExtra(extra + 1);
-            }}
-            component={box.data}
-          />
-        ))}
+        {boxes.length > 0 &&
+          boxes.map((box, k) => (
+            <DragBox
+              key={"preview" + box.id + k}
+              x={box.x}
+              y={box.y}
+              onStop={(e, { x, y }) => {
+                boxes[k].x = x;
+                boxes[k].y = y;
+                setBoxes(boxes);
+                setWithExpiry("position", boxes);
+                // dispatch(setBoxComponents(boxes));
+                setExtra(extra + 1);
+              }}
+              component={box.data}
+            />
+          ))}
       </div>
     </div>
   );
--- src/screen/editor/editorPicker.tsx
+++ src/screen/editor/editorPicker.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import { ComponentButton } from "../../component";
 import { useBoxContext } from "../../context";
-import { getBoxComponents, setBoxComponents } from "../../reduxStore/boxSlices";
-import { useAppDispatch, useAppSelector } from "../../reduxStore/hooks";
-import { getWithExpiry, setWithExpiry } from "../../utils/storeData";
+// import { getBoxComponents, setBoxComponents } from "../../reduxStore/boxSlices";
+// import { useAppDispatch, useAppSelector } from "../../reduxStore/hooks";
+import {
+  deleteStorage,
+  getWithExpiry,
+  setWithExpiry,
+} from "../../utils/storeData";
 import { componentData } from "./constant";
 
 export const EditorPicker = () => {
@@ -34,6 +38,11 @@ export const EditorPicker = () => {
     }
   };
 
+  const onDeleteComponentData = () => {
+    deleteStorage("position");
+    boxCtx?.setExtra(boxCtx.extra + 1);
+  };
+
   return (
     <div className="w-3/12 px-4">
       {/* <input /> */}
@@ -46,6 +55,12 @@ export const EditorPicker = () => {
           addComponent={() => addComponent(i.component)}
         />
       ))}
+      <button
+        onClick={onDeleteComponentData}
+        className="bg-red-600 text-white py-2 w-full rounded-md shadow-xl mt-2"
+      >
+        Clear All
+      </button>
     </div>
   );
 };
